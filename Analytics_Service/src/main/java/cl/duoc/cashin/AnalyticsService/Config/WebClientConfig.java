fa0e5f3dcede3ad3b88ebef25124e29a8a6a1096
Carlos <carlos.pinilla20@gmail.com>
2026-05-30 06:22:04 -0400

    Implementacion de seguridad / agregar JwtFilter y registrar en WebClientConfig

--- Analytics_Service/src/main/java/cl/duoc/cashin/AnalyticsService/Config/WebClientConfig.java
+++ Analytics_Service/src/main/java/cl/duoc/cashin/AnalyticsService/Config/WebClientConfig.java
@@ -4,9 +4,23 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebClientConfig {
+public class WebClientConfig implements WebMvcConfigurer {
+
+    private final JwtFilter jwtFilter;
+
+    public WebClientConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtFilter)
+                .addPathPatterns("/api/v1/**");
+    }
 
     @Bean
     @Qualifier("expenseWebClient")
