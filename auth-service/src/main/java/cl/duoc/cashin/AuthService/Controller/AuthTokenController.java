583b9f849d8b9247c972df8783663948793776a2
Carlos <carlos.pinilla20@gmail.com>
2026-05-30 16:37:00 -0400

    Se cambia logout para recibir token por header

--- auth-service/src/main/java/cl/duoc/cashin/AuthService/Controller/AuthTokenController.java
+++ auth-service/src/main/java/cl/duoc/cashin/AuthService/Controller/AuthTokenController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,8 +35,8 @@ public class AuthTokenController {
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(@RequestParam String token) {
-
+    public ResponseEntity<String> logout(@RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.substring(7);
         authTokenService.logout(token);
         return ResponseEntity.ok("Sesion cerrada exitosamente"); // HTTP 200
     }
