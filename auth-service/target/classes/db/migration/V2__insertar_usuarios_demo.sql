23dc85c6c90c4b7789fc2c457b87134dec68d798
safagod <st.dorta@duocuc.cl>
2026-06-28 23:46:40 -0400

    aplicacion correctamente el .gitignore

--- auth-service/target/classes/db/migration/V2__insertar_usuarios_demo.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `user` (id_user, activo, email, fecha_registro, nombre, password_hash, presupuesto_mensual, telefono) 
-VALUES 
-(2, 1, 'carlosp@gmail.com', '2026-05-26', 'Carlos Pinilla', 'Hola123', 1000000, '56912345678'),
-(3, 1, 'stevenD@gmail.com', '2026-05-26', 'Steven Dorta', 'Chao123456', 5000000, '56978945612'),
-(4, 1, 'mariaP@gmail.com', '2026-05-27', 'Maria Pinilla', 'Estrella25', 2000000, '56978945999'),
-(5, 1, 'juanP@gmail.com', '2026-06-21', 'Juan Perez', 'Estadio123', 3000000, '56922063389');
